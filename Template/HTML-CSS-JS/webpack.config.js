ebffc45fa4e54bc4af7d13db4727ff717452911e
neocris <neocris@users.noreply.github.com>
2020-10-23 22:44:05 +0100

    webpack config for css modules support
    config for adding css modules support to extend the site with other css frameworks for some widgets

---
+++ Template/HTML-CSS-JS/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const webpack = require('webpack');
+
+
+
+
+/*
+ * SplitChunksPlugin is enabled by default and replaced
+ * deprecated CommonsChunkPlugin. It automatically identifies modules which
+ * should be splitted of chunk by heuristics using module duplication count and
+ * module category (i. e. node_modules). And splits the chunks…
+ *
+ * It is safe to remove "splitChunks" from the generated configuration
+ * and was added as an educational example.
+ *
+ * https://webpack.js.org/plugins/split-chunks-plugin/
+ *
+ */
+
+/*
+ * We've enabled TerserPlugin for you! This minifies your app
+ * in order to load faster and run less javascript.
+ *
+ * https://github.com/webpack-contrib/terser-webpack-plugin
+ *
+ */
+
+const TerserPlugin = require('terser-webpack-plugin');
+
+
+
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [new webpack.ProgressPlugin()],
+
+  module: {
+    rules: [{
+      test: /.css$/,
+
+      use: [{
+        loader: "style-loader"
+      }, {
+        loader: "css-loader",
+
+        options: {
+          sourceMap: true,
+          modules: true,
+          importLoaders: 1
+        }
+      },{
+        loader: 'postcss-loader'
+      }]
+    }]
+  }
+}
